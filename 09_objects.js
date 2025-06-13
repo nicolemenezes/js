91c6d4c2d8e45b66227ef5bf578d813b0dc89fe4
nicolemenezes <nicmenezes35@gmail.com>
2025-06-13 11:22:18 +0530

    objects in js

--- 09_objects.js
+++ 09_objects.js
@@ -28,3 +28,35 @@ jsuser.greeting = function()
 //console.log(jsuser.greeting)// undefined
 console.log(jsuser.greeting())
 
+
+
+//*************PART 2************************ */
+
+
+// const instauser = new object()
+
+    const instauser ={}
+instauser.name = "ria"
+instauser.isloggedin= true
+instauser.userid = "123xyz"
+
+
+//console.log(instauser)
+
+const regularuser ={
+    userid:"hi34",
+    fullname:{
+userfullname: {
+    firstname:"nicole",
+    lastname:"menezes"
+}
+    }
+}
+console.log(regularuser.fullname)
+
+const obj1 ={1:"a" ,2:"b"}
+const obj2 ={3:"a" ,4:"b"}
+//const obj3 =Object.assign({},obj1 ,obj2)//{} - target ; obj1,obj2- source
+
+const obj3 ={...obj1,...obj2}
+console.log(obj3);
