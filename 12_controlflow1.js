3d7ace312701b78571888bf186bd92b444f19079
nicolemenezes <nicmenezes35@gmail.com>
2025-06-18 00:54:59 +0530

    control flow 1

---
+++ 12_controlflow1.js
@@ -0,0 +1,39 @@
+//if
+
+if (2!=3){
+
+ //   console.log("yes")
+
+}
+
+const temperature = 42
+if(temperature>50){
+  //  console.log("temperature is more than 50")
+}
+// console.log("temperature is below 50")
+
+// < , > , <= , >= , == ,!= ,===, !==
+
+const score = 200
+
+if(score>100){
+    const power ="fly"
+   // console.log(`user power is: ${power}`);
+}
+
+//console.log(`user power is: ${power}`);// this wont be executed because its outside the scope
+
+
+const balance = 1100
+if (balance < 500) {
+     console.log("less than 500");
+ } else if (balance < 750) {
+    console.log("less than 750");
+    
+} else if (balance < 900) {
+    console.log("less than 750");
+    
+ } else {
+     console.log("less than 1200");
+
+ }
